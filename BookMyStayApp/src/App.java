1301f6e8c8e0c1c73ae67ffe931e2416ab1a89ec
keerthi-manasvi <manjumanasvi.k@gmail.com>
2026-03-20 16:08:40 +0530

    UC1

---
+++ BookMyStayApp/src/App.java
@@ -0,0 +1,23 @@
+/**
+ * UseCase1 Book My Stay App
+ *
+ * This class represents the entry point of the Hotel Booking Management System.
+ * It demonstrates how a Java application begins execution and prints a welcome message.
+ *
+ * @author STEP Training
+ * @version 1.0
+ */
+public class App {
+
+    /**
+     * The main method is the entry point of the application.
+     * JVM invokes this method when the program starts.
+     *
+     * @param args command-line arguments (not used here)
+     */
+    public static void main(String[] args) {
+        // Print welcome message
+        System.out.println(" Welcome to Hotel Booking Management System");
+        System.out.println(" System initialized successfully");
+    }
+}
